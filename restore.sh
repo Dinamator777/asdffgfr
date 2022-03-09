3e5bb4e71ca36a8f9f629d266c539ac42f465369
Dinamator777 <99319203+Dinamator777@users.noreply.github.com>
2022-03-09 19:44:38 +0100

    Initial commit

---
+++ restore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y autossh
+wget -O minecraft_server.jar https://papermc.io/api/v1/paper/1.16.5/latest/download
+unzip server.zip
+#wget -O plugins/IllegalStack.jar https://api.spiget.org/v2/resources/44411/download
+echo eula=true>eula.txt
+unzip -P $PEM pem.zip
+rm -rf pem.zip
+mkdir ~/.ssh/
+mv ayunalt.git-craft.pem ~/.ssh/
+chmod 600 ~/.ssh/ayunalt.git-craft.pem
+#sudo chmod +x pktriot
+#echo $PKTRIOT>pktriot.json
---
+++ server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#./pktriot --config ./pktriot.json start > /dev/null &
+autossh -M 20000 -o StrictHostKeychecking=no -i ~/.ssh/ayunalt.git-craft.pem ayunalt.git-craft@portmap.io -N -R 23063:localhost:25565 &
+echo Starting!
+{
+    sleep 5h
+    pkill java
+    rm -rf minecraft_server.jar
+} &
+for i in {1..10}; do
+    java -Xmx4096M -Xms4096M -jar minecraft_server.jar nogui > /dev/null
+    sleep 1s
+    echo Crash $1/10
+done
